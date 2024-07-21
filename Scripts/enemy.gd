a60e8d6ac89b39b6cc009bd4c7480f569fec64f8
Batuhan Bayram <bbayramgamedev@gmail.com>
2024-07-21 22:11:00 +0300

    Teleport
    added teleporting to the game and fixed some bugs.

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -8,12 +8,12 @@ func _physics_process(delta):
 	var distance_to_player = sqrt(pow(global_position.x - player.global_position.x,2)
 	+ pow(global_position.y - player.global_position.y,2))
 	
-	#if distance_to_player < 250.0:
-		#var direction = global_position.direction_to(player.global_position)
-		#velocity = direction * 200.0
-		#move_and_slide()
+	if distance_to_player < 250.0:
+		var direction = global_position.direction_to(player.global_position)
+		velocity = direction * 100.0
+		move_and_slide()
 		
-	var collision_info = move_and_collide(velocity * delta)
-	if collision_info:
-		if collision_info.get_collider() == player:
-			player.take_damage(20)
+	#var collision_info = move_and_collide(velocity * delta)
+	#if collision_info:
+		#if collision_info.get_collider() == player:
+			#player.take_damage(20)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -13,26 +13,23 @@ var target_position = Vector2()
 
 
 func _physics_process(_delta):
-	#var direction_x = Input.get_axis("ui_left", "ui_right")
-	#var direction_y = Input.get_axis("ui_up", "ui_down")
-	#if direction_x:
-		#velocity.x = direction_x * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-	#if direction_y:
-		#velocity.y = direction_y * SPEED
-	#else:
-		#velocity.y = move_toward(velocity.y, 0, SPEED)
-	#move_and_slide()
+	var direction_x = Input.get_axis("ui_left", "ui_right")
+	var direction_y = Input.get_axis("ui_up", "ui_down")
+	if direction_x:
+		velocity.x = direction_x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if direction_y:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	move_and_slide()
 	
 	if Input.is_action_just_pressed("left_click"): # Teleport
 		click_position = get_global_mouse_position()
-		
-		if position.distance_to(click_position) > 3:
-			target_position = (click_position - position).normalized()
-			velocity = target_position * 100000
-			move_and_slide()
-	
+		position = click_position
+		move_and_slide()
+
 	
 	
 func _input(event):
