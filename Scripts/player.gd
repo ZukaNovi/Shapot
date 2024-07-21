7717118f54907af9bce30e64c3feb26993a099ab
Burak Demirkaya <buraqdmrky@gmail.com>
2024-07-21 21:40:10 +0300

    Added player teleport

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,19 +8,33 @@ const SPEED = 200.0
 const MAX_HP = 100.0
 var currentHP = MAX_HP
 
+var click_position = Vector2()
+var target_position = Vector2()
+
 
 func _physics_process(_delta):
-	var direction_x = Input.get_axis("ui_left", "ui_right")
-	var direction_y = Input.get_axis("ui_up", "ui_down")
-	if direction_x:
-		velocity.x = direction_x * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if direction_y:
-		velocity.y = direction_y * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-	move_and_slide()
+	#var direction_x = Input.get_axis("ui_left", "ui_right")
+	#var direction_y = Input.get_axis("ui_up", "ui_down")
+	#if direction_x:
+		#velocity.x = direction_x * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+	#if direction_y:
+		#velocity.y = direction_y * SPEED
+	#else:
+		#velocity.y = move_toward(velocity.y, 0, SPEED)
+	#move_and_slide()
+	
+	if Input.is_action_just_pressed("left_click"): # Teleport
+		click_position = get_global_mouse_position()
+		
+		if position.distance_to(click_position) > 3:
+			target_position = (click_position - position).normalized()
+			velocity = target_position * 100000
+			move_and_slide()
+	
+	
+	
 func _input(event):
 	if event.is_action_pressed("ui_up"):
 		$AnimatedSprite2D.play("MoveUp")
