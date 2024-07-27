60c7a404fc9b43e835bd171c6f13cfaaa5ad7500
Batuhan Bayram <bbayramgamedev@gmail.com>
2024-07-27 21:25:15 +0300

    Added home.

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,18 +1,17 @@
 extends Node2D
 
 @onready var pause_menu = $Player/PauseMenu
-@onready var point_light_2d = $PointLight2D
-@onready var dungeon_entry = $DungeonEntry
 @onready var day_night_cycle = $DayNightCycle
+
 var paused = false
 func _ready():
 	if ConfigFileHandler.count_scene == 2:
 		day_night_cycle.scene_name = "Game"
 		ConfigFileHandler.count_scene = 0
 	ConfigFileHandler.count_scene = 1 + ConfigFileHandler.count_scene
-	dungeon_entry.scene_name = "Game"
 func _process(_delta):
 	if Input.is_action_just_pressed("escape"):
+		print("zort")
 		pauseMenu()
 		
 func pauseMenu():
@@ -23,4 +22,4 @@ func pauseMenu():
 		pause_menu.show()
 		Engine.time_scale = 0
 
-	paused = !paused		
+	paused = !paused
