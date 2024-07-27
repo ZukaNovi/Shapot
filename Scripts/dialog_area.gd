e8cddd3a363661b6a197d1ba0e96faa6c7d8f4e7
erendmrky <eren.demirkaya01@gmail.com>
2024-07-28 00:47:29 +0300

    Dialog added
    dialog position must be fixed and need to save scenes

---
+++ Scripts/dialog_area.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@export var text_key: String = ""
+
+var area_active: bool = false
+
+func _physics_process(_delta):
+	if area_active:
+		SignalBus.emit_signal("display_dialog", text_key)
+	if ConfigFileHandler.load_dialog_settings() == 1:
+		queue_free()
+
+
+func _on_DialogArea_area_entered(area):
+	area_active = true
+
+
+func _on_DialogArea_area_exited(area):
+	area_active = false
