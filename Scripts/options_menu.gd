a318253598fd1463e6e23d29c2d32d46286e4a8c
Batuhan Bayram <bbayramgamedev@gmail.com>
2024-07-29 03:20:13 +0300

    Option Button added.
    Option button added for 3 different type of colour options(not working atm).

--- Scripts/options_menu.gd
+++ Scripts/options_menu.gd
@@ -1,9 +1,15 @@
 extends Control
+#var red = preload("res://Assets/Sprites/Hud/Hud_red.png")
+#var orange = preload("res://Assets/Sprites/Hud/Hud_orange.png")
+#var original = preload("res://Assets/Sprites/Hud/Hud_original.png")
+#var DUNGEON_SCENE = preload("res://Scenes/dungeon.tscn")
+#@onready var potion_hud = get_node("Dungeon/CanvasLayer/PotionHUD")
+
+#BU RENK SECME OLAYI OLCAK
 
 @onready var fullscreen_checkbox = $VBoxContainer/Fullscreen
 @onready var vsync_checkbox = $VBoxContainer/VSync
 @onready var volume_slider = $VBoxContainer/VolumeSlider
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var video_settings = ConfigFileHandler.load_video_settings()
@@ -36,3 +42,14 @@ func _on_v_sync_toggled(toggled_on):
 func _on_volume_slider_drag_ended(value_changed):
 	if value_changed:
 		ConfigFileHandler.save_audio_settings("master_volume", volume_slider.value / 100)
+
+
+func _on_option_button_item_selected(index):
+	#match index:
+		#0:
+			#potion_hud.texture("original")
+		#1:
+			#potion_hud.texture("red")
+		#2:
+			#potion_hud.texture("orange")
+	pass
